835a5d128bdece61e244dcbba7d94b8c1fc72b1b
jhansarmiento <jhancsn1@gmail.com>
2025-04-25 10:58:22 -0500

    Initial commit

---
+++ src/inspeccion_fitosanitaria/Main.java
@@ -0,0 +1,9 @@
+package inspeccion_fitosanitaria;
+
+public class Main {
+
+	public static void main(String[] args) {
+        System.out.println("¡Bienvenido al sistema de Inspección Fitosanitaria!");
+	}
+
+}
